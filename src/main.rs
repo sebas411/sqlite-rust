e3bb52dc6cb0d3b8c863d1eeed67e64f7a09d17f
sebas411 <sebasma411@gmail.com>
2025-11-13 15:57:47 -0600

    Print number of tables

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,15 @@ fn main() -> Result<()> {
             // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
+            let mut buffer = Vec::new();
+            buffer.resize(page_size as usize, 0u8);
+
+            file.read_exact(&mut buffer[100..])?;
+
+            let table_num = u16::from_be_bytes([buffer[100+3], buffer[100+4]]);
+
             println!("database page size: {}", page_size);
+            println!("number of tables: {}", table_num);
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
